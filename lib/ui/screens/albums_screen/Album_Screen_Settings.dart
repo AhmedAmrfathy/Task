9089c54f4dcbf8eeb30b1462750d4d216ad97149
ShroukAbdelrazik <63430895+ShroukAbdelrazik@users.noreply.github.com>
2022-02-15 13:15:54 +0200

    15/2/

---
+++ lib/ui/screens/albums_screen/Album_Screen_Settings.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shanbwrog/Settings/MySettings.dart';
+import 'package:shanbwrog/ui/screens/user_Screen/User_Screen.dart';
+
+class AlbumScreenSettings {
+  //header settings
+  static String title = 'Albums';
+  static TextStyle title_Style = TextStyle(
+    fontSize: 24,
+    color: Colors.blueAccent,
+  );
+  static Widget trailing(BuildContext context){
+    return TextButton.icon(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (ctx){
+            return UserScreen();
+          }));
+        },
+        icon: Icon(
+          Icons.account_circle_outlined,
+          color: MySettings.maincolor,
+          size: 24,
+        ),
+        label: Text('User'));
+  }
+  }
